fcffd8ea4d8437da175bd80dd803d86bf0f79579
edu-alasos <alansebastian.soszynski@skola.taby.se>
2026-05-08 11:29:47 +0200

    overflow-y fix products, update currency.js

--- javascript/currency.js
+++ javascript/currency.js
@@ -1,24 +1,35 @@
 api = "https://v6.exchangerate-api.com/v6/3a841749723118f07780902f/latest/USD"
+apiDebouncePeriod = 600000
 
 async function getData() {
-    const response = await fetch(api)
-    const data = await response.json()
-    console.log(data)
-    conversionRates = data["conversion_rates"]
-    console.log(conversionRates)
-
-    let rates = JSON.parse(localStorage.getItem("rates") || '[]')
-    rates.push({conversionRates})
-    localStorage.setItem("rates", JSON.stringify(rates))
-
     let date = new Date
     let time = date.getTime()
 
+    let rates = JSON.parse(localStorage.getItem("rates") || '[]')
+
     let lastFetch = JSON.parse(localStorage.getItem("lastFetchConversions") || '[]')
-    lastFetch.push({time})
+    let previousFetch = lastFetch
+    lastFetch = {time}
     localStorage.setItem("lastFetch", JSON.stringify(lastFetch))
 
-    return conversionRates
+    console.log(rates)
+
+    if (lastFetch - previousFetch > apiDebouncePeriod || rates.length === 0)  {
+        const response = await fetch(api)
+        const data = await response.json()
+        console.log(data)
+        conversionRates = data["conversion_rates"]
+        console.log(conversionRates)
+
+        rates = ({conversionRates})
+        localStorage.setItem("rates", JSON.stringify(rates))
+
+        return conversionRates
+    }
+
+    else {
+        console.log("Didn't fetch api, recently fetched")
+    }
 }
 
 getData()
